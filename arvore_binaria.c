02f024671c04a7e630779eaadfc3336909339545
gil-a-a <98371042+gil-a-a@users.noreply.github.com>
2022-06-25 17:47:28 -0300

    Update arvore_binaria.c

--- arvore_binaria.c
+++ arvore_binaria.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-//nó da árvore
+//nÃ³ da Ã¡rvore
 typedef struct node{
 	int info;
 	struct node *esq;
@@ -11,60 +11,41 @@ typedef struct node{
 
 int a = 0; //var global pra testar
 
-//funções
+////////////////////funÃ§Ãµes///////////////////////
 struct node* cria_no();
 void percorre_arv(Node *raiz);
-struct node* insere_na_arv(Node *no, int num)
-{
-	if (no == NULL){
-		no = cria_no(num);
-		return no;
-	}
-	
-	if (num < no->info)
-		insere_na_arv(no->esq, num);
-	else
-		insere_na_arv(no->dir, num);
-}
-
-struct node* coloca_nums_na_arv(Node *raiz)
-{
-	int aux, i;
-	srand(time(NULL));
-	
-	raiz = cria_no(rand() % 11);
-	printf("%d ", raiz->info);
-	
-	i = 1;
-	while (i <= 3){
-		aux = rand() % 11;
-		printf("%d ", aux);
-		
-		if (aux < raiz->info){
-			raiz->esq = insere_na_arv(raiz->esq, aux);
-		}
-		else{
-			raiz->dir = insere_na_arv(raiz->dir, aux);
-		}
-		
-		i++;
-	}
-	
-	return raiz;
-}
-
+void InsEsq(Node *aux, int x);
+void InsDir(Node *aux, int x);
+void insere_na_arv(Node **no, int num);
+void tem_repetido(Node *raiz, int num, int *aux);
+void coloca_nums_na_arv(Node **raiz);
+/////////////////////////////////////////////////
 
 int main ()
 {
-	Node *raiz;
+	Node *raiz, *raiz_2;
 	
-	raiz = coloca_nums_na_arv(raiz);
+	raiz = cria_no(5);
+	InsEsq(raiz, 4);
+	InsDir(raiz, 7);
+	
+	InsEsq(raiz->esq, 2);
+	InsDir(raiz->esq, 3);
+	
+	InsEsq(raiz->dir, 6);
+	InsDir(raiz->dir, 8);
 	
 	percorre_arv(raiz);
+	printf("\n---------Nova arvore---------\n");
+	
+	coloca_nums_na_arv(&raiz_2);
+	printf("\nArvore pre-ordem: ");
+	percorre_arv(raiz_2);
 	
 	return 0;
 }
 
+/////////////////////////////////////////////////
 struct node* cria_no(int info)
 {
 	Node *aux;
@@ -80,9 +61,78 @@ struct node* cria_no(int info)
 void percorre_arv(Node *raiz)
 {
 	if (raiz != NULL){
-		printf("%d ", raiz->info);
+	    printf("%d ", raiz->info);
 		percorre_arv(raiz->esq);
 		percorre_arv(raiz->dir);
 	}
 }
 
+void InsEsq(Node *aux, int x)
+{
+    if (aux->esq != NULL)
+        printf("Operacao ilegal\n");
+    else
+        aux->esq = cria_no(x);
+}
+
+void InsDir(Node *aux, int x)
+{
+    if (aux->dir != NULL)
+        printf("Operacao ilegal\n");
+    else
+        aux->dir = cria_no(x);
+}
+
+void insere_na_arv(Node **no, int num)
+{
+	if (*no == NULL){
+	    *no = cria_no(num);
+	    return;
+	}
+	
+	if (num <= (*no)->info)
+		insere_na_arv(&(*no)->esq, num);
+	else
+		insere_na_arv(&(*no)->dir, num);
+}
+
+void tem_repetido(Node *raiz, int num, int *aux)
+{
+    if (raiz == NULL)
+        return;
+    else if (raiz->esq != NULL || raiz->dir != NULL){
+        tem_repetido(raiz->esq, num, aux);
+        tem_repetido(raiz->dir, num, aux);
+    }
+    if (raiz->info == num)
+        *aux = 1;
+    else
+        return;
+}
+
+void coloca_nums_na_arv(Node **raiz)
+{
+	int i, aux, aux_2;
+	srand(time(NULL));
+	
+	*raiz = cria_no(rand() % 31);
+	printf("Elemento da raiz: %d\n", (*raiz)->info);
+	
+	i = 1;
+	while (i <= 5){
+		do{
+		    aux_2 = 0;
+		    aux = rand() % 31;
+		    tem_repetido(*raiz, aux, &aux_2);   //coloquei uma flag por referÃªncia, pq foi mais fÃ¡cil fazer assim
+		}while(aux_2);
+		printf("Elemento %d: %d\n", i, aux);
+		
+		if (aux <= (*raiz)->info)
+			insere_na_arv(&(*raiz)->esq, aux);
+		else
+			insere_na_arv(&(*raiz)->dir, aux);
+		
+		i++;
+	}
+}
+/////////////////////////////////////////////////
